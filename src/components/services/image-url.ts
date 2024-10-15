6a8f70742b1c1a85a1f72cd7a1164a56f3c12726
Zoltán Szűcs <zoltan76szucs@gmail.com>
2024-10-15 11:36:53 +0200

    Handel game no Image

--- src/components/services/image-url.ts
+++ src/components/services/image-url.ts
@@ -1,5 +1,8 @@
+
+import noImage from '../assets/clipart2277827.webp'
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400" + url.slice(index);
